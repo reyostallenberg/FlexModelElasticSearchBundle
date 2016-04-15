0e17fd92152eaec3fe5ce2590997491443e2c438
Niels Nijens <nijens.niels@gmail.com>
2016-04-15 15:30:45 +0200

    Add FlexModelElasticsearchBundle class

---
+++ FlexModelElasticsearchBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FlexModel\FlexModelElasticsearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FlexModelElasticsearchBundle.
+ *
+ * @author Niels Nijens <niels@connectholland.nl>
+ */
+class FlexModelElasticsearchBundle extends Bundle
+{
+}
